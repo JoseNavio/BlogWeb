3ee3332eee281315a6be5c61e0e2eb829200a821
JoseNavio <josename91@hotmail.com>
2024-04-29 23:47:24 +0200

    feature: Now form remembers and validate entries

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,18 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    public function register(Request $request){
+    public function register(Request $request)
+    {
         $incomingFields = $request->validate(
             [
-                "username"=> "required",
-                "email"=> "required",
-                "password"=> "required"
+                "username" => [
+                    "required",
+                    "min:3",
+                    "max:20",
+                    //(Table name, column name)
+                    Rule::unique('users', 'username')
+                ],
+                "email" => ["required","email",Rule::unique("users","email")],
+                //Html file inpunt name need to be pizza and pizza_confirmation
+                "password" => ["required","min:3","confirmed"]
             ]
         );
+        User::create($incomingFields);
         return "Hello from the register...";
     }
 }
